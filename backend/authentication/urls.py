7eab74ce0dca4a42a0d5728b2b709e41aa216f12
Wiktor Baura <wiktor.baura@ardigen.com>
2023-01-13 15:00:40 +0100

    Removed unnecessary slash

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import SignUpView, LoginView
 
 urlpatterns = [
-    path("/register", SignUpView.as_view(), name="register"),
-    path("/login", LoginView.as_view(), name="login"),
+    path("register", SignUpView.as_view(), name="register"),
+    path("login", LoginView.as_view(), name="login"),
 ]
