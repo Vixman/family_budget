cea5beb0719e07e56f824d4675de15ca15527e8b
Wiktor Baura <wiktor.baura@ardigen.com>
2023-01-13 15:34:37 +0100

    Introduced Income view

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,6 +21,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/auth", include("authentication.urls")),
+    path("api/income/", include("income.urls")),
     path(
         "docs/",
         schema_view.with_ui("swagger", cache_timeout=0),
