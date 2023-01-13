aa080fd2b356e4367b6b8af2c117d4c3271c55b8
Wiktor Baura <wiktor.baura@ardigen.com>
2023-01-13 14:14:36 +0100

    Added login view, serializer & url

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView, LoginView
 
 urlpatterns = [
     path("/register", SignUpView.as_view(), name="register"),
+    path("/login", LoginView.as_view(), name="login"),
 ]
